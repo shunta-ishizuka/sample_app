9daee1a8542b201fc7199e62ff0fe0dda90f2efe
shunta-ishizuka <shunta.ishizuka@pa-rk.co.jp>
2016-07-07 02:13:34 +0000

    9.7 : Add user edit false test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,15 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功したときの処理
+    else
+      render 'edit'
+    end
+  end
 
   private
 
