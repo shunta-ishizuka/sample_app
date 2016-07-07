e5d96f8d2d4b5218319aa4ef9ad8adfaca43fd6c
shunta-ishizuka <shunta.ishizuka@pa-rk.co.jp>
2016-07-07 03:38:10 +0000

    9.30 : Add frendly forword

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -49,4 +49,15 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
   
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+  
 end
