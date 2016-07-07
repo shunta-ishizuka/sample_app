58de2a9f1b5bd2c31e64fdad52e97a7b72629f38
shunta-ishizuka <shunta.ishizuka@pa-rk.co.jp>
2016-07-07 03:59:19 +0000

    9.37 : Add user path and  gravatar_for

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,14 +5,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: {
-        name: "",
-        email: "user@invalid",
-        password: "foo",
-        password_confirmation: "foo"
-      }
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
   end
   
   test "valid signup information" do
