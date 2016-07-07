0d13f4e2b08f9a35414d62d2b9d74961374cec23
shunta-ishizuka <shunta.ishizuka@pa-rk.co.jp>
2016-07-07 02:08:52 +0000

    9.2 : Add user edit form

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :show, :create]
+  # resources :users, only: [:new, :show, :create]
+  resources :users
   
   root 'static_pages#home'
-  get  'help'   =>  'static_pages#help'
-  get  'about'  =>  'static_pages#about'
-  get  'contact'  =>  'static_pages#contact'
-  get  'signup' =>  'users#new'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  get  'help'       =>  'static_pages#help'
+  get  'about'      =>  'static_pages#about'
+  get  'contact'    =>  'static_pages#contact'
+  get  'signup'     =>  'users#new'
+  get    'login'    =>  'sessions#new'
+  post   'login'    =>  'sessions#create'
+  delete 'logout'   =>  'sessions#destroy'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
