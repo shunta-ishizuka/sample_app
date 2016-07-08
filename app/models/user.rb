4fd946e3ab092091c3efcb410c76aad2b47d39be
shunta-ishizuka <shunta.ishizuka@pa-rk.co.jp>
2016-07-08 02:12:24 +0000

    12.48 : Finish 12

--- app/models/user.rb
+++ app/models/user.rb
@@ -79,10 +79,12 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago
   end
   
-  # 試作feedの定義
-  # 完全な実装は第12章「ユーザーをフォローする」を参照してください。
+  # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
   
   # ユーザーをフォローする
